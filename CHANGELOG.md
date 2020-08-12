ac0765af2975af5158cd7990a15cea79a832150d
Vadim Laletin <rusmiligamer@gmail.com>
2020-08-13 00:11:45 +0700

    Up to 1.2.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+## 1.2.0
+* Update fhirpy
+
 ## 1.1.0
 * Update fhirpy
 
--- aidboxpy/__init__.py
+++ aidboxpy/__init__.py
@@ -8,10 +8,10 @@ from fhirpy.base.resource import BaseResource, BaseReference
 from fhirpy.base.searchset import AbstractSearchSet
 
 __title__ = 'aidbox-py'
-__version__ = '1.1.0'
+__version__ = '1.2.0'
 __author__ = 'beda.software'
 __license__ = 'None'
-__copyright__ = 'Copyright 2019 beda.software'
+__copyright__ = 'Copyright 2020 beda.software'
 
 # Version synonym
 VERSION = __version__
